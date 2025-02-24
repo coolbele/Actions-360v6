296fa415e35583a48f1d57ff5fed0d034f3f36ff
coolbele <64392071+coolbele@users.noreply.github.com>
2025-02-24 08:07:38 +0800

    Update diy2.sh

--- scripts/360V6/diy2.sh
+++ scripts/360V6/diy2.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/haiibo/openwrt-packages.git;master' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small.git;master' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
 
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
