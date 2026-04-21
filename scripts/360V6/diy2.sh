d9e7a4a0a8d9a9009df71aaeb2bc1acbc7e0cf08
coolbele <64392071+coolbele@users.noreply.github.com>
2026-04-21 11:54:55 +0800

    Update diy2.sh

--- scripts/360V6/diy2.sh
+++ scripts/360V6/diy2.sh
@@ -14,6 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
+# Add a feed source
+
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
 #echo 'src-git MyPackages https://github.com/wwz09/MyPackages.git;main' >>feeds.conf.default
@@ -21,3 +25,15 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>fee
 #echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
 #echo 'src-git adguardhome https://github.com/xiaoxiao29/luci-app-adguardhome;master' >>feeds.conf.default
 #echo 'src-git 360v6 https://github.com/wwz09/360v6-packages' >>feeds.conf.default
+
+#echo 'src-git MyPackages https://github.com/wwz09/MyPackages.git;main' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+#echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
+#echo 'src-git adguardhome https://github.com/xiaoxiao29/luci-app-adguardhome;master' >>feeds.conf.default
+#echo 'src-git 360v6 https://github.com/wwz09/360v6-packages' >>feeds.conf.default
+git clone https://github.com/laipeng668/luci-app-gecoosac.git package/luci-app-gecoosac
+
+# 在 diy-part2.sh 中
+# 下载指定 Commit 的压缩包并解压到 package 目录
+mkdir -p package/kenzok8_old
+curl -L https://github.com/kenzok8/openwrt-packages/archive/c587b23.tar.gz | tar -xz -C package/kenzok8_old --strip-components=1
